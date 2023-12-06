1a8e5528efe2036aa3a7e50e85ba20e8c7caf584
Till Gartner <tgartner@TILLGMAC2.mgm-edv.de>
2023-12-06 17:43:03 +0100

    Day06 part2 ;)

--- day06_wait_for_it/aoc202306.py
+++ day06_wait_for_it/aoc202306.py
@@ -8,10 +8,10 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from day01_trebuchet.utils import get_logger
 
-def parse(puzzle_input):
+def parse(raw_data):
     """Parse input."""
     logger = get_logger(parse.__name__, logging.INFO)
-    lines = [line for line in puzzle_input.split("\n")]
+    lines = [line for line in raw_data.split("\n")]
     logger.info(f"Lines: {lines}")
     times_words = [word for word in lines[0].split()]
     del times_words[0]
@@ -21,6 +21,21 @@ def parse(puzzle_input):
     del distances_words[0]
     distances = [int(word) for word in distances_words]
     return {"time": times, "distance": distances}
+
+def parse2(raw_data):
+    """Parse input for puzzle part2"""
+    logger = get_logger(parse2.__name__, logging.INFO)
+    interims_data = parse(raw_data)
+    logger.info(f"Interims data: {interims_data}")
+    time_strings = [str(time) for time in interims_data["time"]]
+    time_string = "".join(time_strings)
+    long_time = int(time_string)
+    distance_strings = [str(distance) for distance in interims_data["distance"]]
+    distance_string = "".join(distance_strings)
+    long_distance = int(distance_string)
+    new_input = {"time": [long_time], "distance": [long_distance]}
+    logger.info(f"New input: {new_input}")
+    return new_input
 
 def travel_distances(race_duration, time_pressed):
     speed = time_pressed
@@ -35,25 +50,35 @@ def number_of_possible_faster_options(race_duration, farthest_distance):
             faster_options += 1
     return faster_options
 
-def part1(data):
+def part1(input):
     """Solve part 1."""
     product = 1
-    for i in range(len(data["time"])):
-        faster_options = number_of_possible_faster_options(data["time"][i], data["distance"][i])    
+    for i in range(len(input["time"])):
+        faster_options = number_of_possible_faster_options(input["time"][i], input["distance"][i])    
         product *= faster_options
     return product
 
 
 def part2(data):
     """Solve part 2."""
-    sum = 0
-    for line in data:
-        sum += get_number_of_line_including_written_numbers(line)
-    return sum
+    logger = get_logger(part2.__name__, logging.INFO)
+    product = 1
+    logger.info(f"Input: {input}")
+    for i in range(len(input["time"])):
+        faster_options = number_of_possible_faster_options(input["time"][i], data["distance"][i])    
+        product *= faster_options
+    return product
 
 if __name__ == "__main__":
-    puzzle_input = pathlib.Path("./input1.txt").read_text().strip()
-    input = parse(puzzle_input)
+    logger = get_logger("main", logging.INFO)
+
+    raw_data = pathlib.Path("./input1.txt").read_text().strip()
+    logger.info(f"Raw_data: {raw_data}")
+
+    input = parse(raw_data)
     solution1 = part1(input)
     print(f"Solution 1: {solution1}")
-    #solution2 = part2(puzzle_input)
+
+    input2 = parse2(raw_data)
+    solution2 = part1(input2)
+    print(f"Solution 2: {solution2}")
